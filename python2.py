07cd3d128f8547cb0c114183422dbb1ee11400a9
varshithdepa45 <varshithreddy987@gmail.com>
2025-03-06 20:59:37 +0530

    2nd commit

---
+++ python2.py
@@ -0,0 +1,4 @@
+print('hey\'s,"mac"') # use \ for skipping special meaning
+print('hey'+'hey') # '' use seperately for doing operations
+print(10*'hey\t') # '' use it seperate
+print(r'hey\mac') # r= raw string before '' for skipping string special meaning
