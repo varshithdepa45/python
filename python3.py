372c4dfcfd5e38c77940af450e7749969450d8f8
varshithdepa45 <varshithreddy987@gmail.com>
2025-03-06 21:35:01 +0530

    commit-3

---
+++ python3.py
@@ -0,0 +1,17 @@
+x = 10
+y=x+9
+print(y) # '' should not be used
+name='jaibhavani'
+print(name) # direct print
+print (name[0]) # [] for fetching the digit in the given position
+print(name[0]+name[2])
+print(name[-1]) # -ve fetches from right
+print(name[0:1]) # : in [] is used to print from 0 to 1 places here
+print(name[0:2]) #: in [] is used to print from 0 to 2 places here
+print(name[1:]) # exclude before 1 digit places
+print(name[2:]) # exclude before 2 digit places
+print(name[:2]) # include before 2 
+print(name[0:2]) # x: = includes :x excludes, x:y= x included y excluded
+print(len(name)) # len is function used for length measuring used inside print 
+
+
