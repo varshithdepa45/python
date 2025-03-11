a15e5b6de6244fe62c3d37397417ec60ab03bbfc
varshithdepa45 <varshithreddy987@gmail.com>
2025-03-11 08:11:00 +0530

    commit

---
+++ python6.py
@@ -0,0 +1,10 @@
+#variables 2
+num = 3
+print(id(num)) #id show the address
+a=10
+b=a
+print(id(a))
+print(id(b))
+print(id(10))
+#all have the same address
+
---
+++ python7.py
@@ -0,0 +1,17 @@
+#data types
+# 1)none =null
+#2 numeric (int,float,complex,bool)
+num=5 #int
+num=5.55 #float
+print(type(num)) #type() tells the type of data type
+num=6+9j #complex num (j is imaginary num)
+print(type(num))
+a=5.5
+b=int(a)
+print(b) #can be changed directly
+k=7
+c= complex(b,k) #complex is used to assign or give values in complex nums
+print(c)
+bool=b>k
+print(bool) #bool checks (use bool)
+
