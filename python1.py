364af5dafece9caaf1b72dc1591f24e52b5c8e40
varshithdepa45 <varshithreddy987@gmail.com>
2025-05-06 21:18:33 +0530

    1st

---
+++ python1.py
@@ -0,0 +1,27 @@
+ss='jijiji11'
+print(ss[:2])
+c='hey dude'
+b=" dude"
+print(c+b)
+print(c+b*2)
+c.upper() #upper case
+print(c)
+c.lower() #lower case
+print(c)
+print(c.split()) #split
+list1=['hey','dude']
+list1.append('4')#append or add
+print(list1)
+list1.pop(1)#pop
+print(list1)
+list1.remove('hey')#remove
+print(list1)
+list1.insert(0,'hey')#insert
+print(list1)
+list1.sort()#sort
+print(list1)
+list1.reverse()#reverse
+print(list1)
+type(list1)
+list1.clear()#clear
+print(list1)
