b44b127512634e5e4eb5438a074ee737949b8fa8
varshithdepa45 <varshithreddy987@gmail.com>
2025-05-06 19:31:14 +0530

    1st push

--- python2.py
+++
@@ -1,4 +0,0 @@
-print('hey\'s,"mac"') # use \ for skipping special meaning
-print('hey'+'hey') # '' use seperately for doing operations
-print(10*'hey\t') # '' use it seperate
-print(r'hey\mac') # r= raw string before '' for skipping string special meaning
--- python5.py
+++
@@ -1,7 +0,0 @@
-tup=(1,2,3,4)
-print(tup[1])
-print(tup.count)
-s={1,4,2,8,4,5,6}
-print(s)  #it will auto sort in {} not in an order but randomly
-#[] does not works
-
