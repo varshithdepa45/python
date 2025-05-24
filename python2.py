462ba9c04a0914853df077f1e16e5e86057677ca
varshithdepa45 <varshithreddy987@gmail.com>
2025-05-24 16:46:38 +0530

    commit

---
+++ python2.py
@@ -0,0 +1,11 @@
+#format
+print("Salaar {b} {a}".format(a="Raisaar", b="Devartha"))
+value=122/22
+print("the value is {v:.3f}".format(v=value))#for certain digits
+print(f"the value is{value:.2f}")
+#dictionaries
+my_dictionary={'age':'18','height':'5.8'}
+print(my_dictionary['age'])
+
+
+
