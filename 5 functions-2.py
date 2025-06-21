28fcde5cec2182bbf4bd3b72fb07bcd598eb1be6
varshithdepa45 <varshithreddy987@gmail.com>
2025-06-21 19:16:21 +0530

    commit

--- 5 functions-2.py
+++ 5 functions-2.py
@@ -3,3 +3,16 @@ for prices in ticket_prices:
     print(prices)
 for movie,price in ticket_prices:
     print(movie)
+#lamda
+def square(num):
+    return num**2
+nums=[1,2,3,4]
+for item in map(square,nums):
+    print(item)
+def slice(mystring):
+    if len(mystring)%2==0:
+        return "even"
+    else:
+        return mystring[0]
+names=['salaar','devaratha','raisaar']
+print(list(map(slice,names)))
